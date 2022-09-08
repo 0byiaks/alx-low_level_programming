0d415f5da3808ee13df342b5365568c72f2adb06
0byiaks <austinbale667@gmail.com>
2022-09-08 14:33:30 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#include <stdio.h>
+
+/**
+* main - Print the string in the put function
+*
+*Return: 0
+*/
+int main (void) /*main func*/
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
