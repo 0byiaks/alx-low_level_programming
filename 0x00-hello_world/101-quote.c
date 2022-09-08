c7dd4bba413e9be02c4f728ae86d23d41b601e67
0byiaks <austinbale667@gmail.com>
2022-09-08 15:47:00 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,8 @@
 #include <unistd.h>
 
 /**
- *
  *main - program starts and ends here
  *
- *takes no arguments
  *Description: prints a string to the stderr stream with write()
  *Return: return value is 1
  */
