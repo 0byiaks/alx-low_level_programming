210029ba0a07fa5514e13fa6cb7362a50ecc87c2
0byiaks <austinbale667@gmail.com>
2022-09-08 14:49:24 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+*Description: using the main function
+* this program print the following below
+*Return:0
+*/
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return(0);
