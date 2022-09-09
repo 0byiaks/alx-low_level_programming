515efd7ac717976eecc61b2fcfa5e7cf528754e1
0byiaks <austinbale667@gmail.com>
2022-09-09 15:47:19 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -entry point of the program
+ *
+ * Takes no arguments
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; I <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar(‘\n’);
+
+	return (0);
+}
