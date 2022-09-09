d9b113f37330c1019f5380b20402f31f48bfbea1
0byiaks <austinbale667@gmail.com>
2022-09-09 15:03:58 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- the Entry point of the program
+ * The program Takes no arg.
+ *
+ * Description: prints a-z and A-Z using a for loop
+ * Return: program always return 0(success)
+ */
+int main(void)
+{
+	char cl;
+	char cu;
+
+	for (cl = 'a'; cl <= 'z'; cl++)
+	{
+		putchar(cl);
+	}
+	for (cu = 'A'; cu <= 'Z'; cu++)
+	{
+		putchar(cu);
+	}
+	putchasr('\n');
+	return (0);
+}
