c790d77501ca2aa64aa667541e508819c4914924
0byiaks <austinbale667@gmail.com>
2022-09-22 19:48:07 -0700

    correction

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,7 +4,7 @@
  * _strncpy - function that copies a string.
  * @n: size of character to contatenate
  * @dest: s1
- * src: s2
+ * @src: s2
  * Return: Always 0.
  */
 char *_strncpy(char *dest, char *src, int n)
